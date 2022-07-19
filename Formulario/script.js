bf5da90cf6ba4b0092c74b8e70858b6e6aaf4307
Guilherme Ramos Ferreira <104803024+GuilhermeRamosFerreira@users.noreply.github.com>
2022-07-19 00:11:31 -0300

    Add files via upload

---
+++ Formulario/script.js
@@ -0,0 +1,10 @@
+
+function enviar() {
+    var myName = window.document.getElementById('nome')
+	var myEmail = window.document.getElementById('email')
+	var myEmpresa = window.document.getElementById('empresa')
+	var myAssunto = window.document.getElementById('assunto')
+	var myTelefone = window.document.getElementByIs('telefone')
+	Nome.innerHTML = (`Seu Nome é ${myName}`)
+}
+
